82c8b0d9025f4c25a862bb0f8dd2a44f0edd09e0
DIOMANDE Manan Yves Lionel <diomandemananyveslionel@MacBook-Pro-de-DIOMANDE.local>
2024-06-05 13:24:14 +0000

    done with overview segment

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,17 +1,38 @@
 import DashboardLayout from '@/layouts/DashboardLayout';
-import { Text } from '@mantine/core';
-import React, { ReactNode } from 'react';
-
-interface DashboardLayoutProps {
-  children: ReactNode;
+import { Button, Group, Stack, Text, Title } from '@mantine/core';
+import React from 'react';
+import { BarChart } from '@mantine/charts';
+import data from '../../mock/barData';
+export default function Overiew() {
+  return (
+    <Stack>
+      <Title>Overview</Title>
+      <Title order={4}>4 used out 100</Title>
+      <BarChart
+        h={600}
+        data={data}
+        dataKey="month"
+        series={[
+          { name: 'Smartphones', color: 'violet.6' },
+          { name: 'Laptops', color: 'blue.6' },
+          { name: 'Tablets', color: 'teal.6' },
+        ]}
+        tickLine="y"
+      />
+      <Title>Quick Actions</Title>
+      <Group gap={100} >
+        {' '}
+        <Group>
+          {' '}
+          <Title order={4}>Create new Template</Title> <Button>create</Button>{' '}
+        </Group>{' '}
+        <Group>
+          {' '}
+          <Title order={4}>Usage Logs</Title> <Button>open</Button>{' '}
+        </Group>{' '}
+      </Group>
+    </Stack>
+  );
 }
 
-export default function Overiew({ children }: DashboardLayoutProps)  {
-  return (
-    <Text>Overview</Text>
-  );
-};
-
-Overiew.getLayout = (page: React.ReactNode) => (
-    <DashboardLayout>{page}</DashboardLayout>
-  );
+Overiew.getLayout = (page: React.ReactNode) => <DashboardLayout>{page}</DashboardLayout>;
