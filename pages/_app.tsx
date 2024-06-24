9ef23edf623b6525df3f3dd4761cd483a852544f
DIOMANDE Manan Yves Lionel <diomandemananyveslionel@MacBook-Pro-de-DIOMANDE.local>
2024-06-24 08:47:44 +0000

    add guard Auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,16 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { MantineProvider } from '@mantine/core';
 import { theme } from '../theme';
-import { ReactElement, ReactNode } from 'react';
+import { ReactElement, ReactNode, useEffect } from 'react';
 import { NextPage } from 'next';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { ModalsProvider } from '@mantine/modals';
 import { Notifications } from '@mantine/notifications';
+import { useRouter } from 'next/router';
+import { authApi } from '@/api/authApi';
+import { AppConfig } from '@/utils/appConfig';
+
 type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
 };
@@ -18,9 +22,26 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
+const screensNameAllowedToMoveToHomePage = ['/login', '/signup'];
+
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page);
+  const router = useRouter();
+
+  useEffect(() => {
+    const userSession = authApi.getUserSession();
+    const pathName = router.pathname;
+    console.log(pathName);
+
+    if (userSession?.accessToken) {
+      if (screensNameAllowedToMoveToHomePage.includes(pathName)) {
+        router.replace('/dashboard');
+      } else {
+        router.replace(pathName);
+      }
+    }
+  }, []);
 
   return (
     <MantineProvider theme={theme}>
--- utils/appConfig.ts
+++ utils/appConfig.ts
@@ -1,25 +1,22 @@
 export enum Environment {
-    DEV_SIMULATOR = "dev-simulator",
-    DEV = "dev",
-    PROD = "prod",
-    STAGING = "staging",
-  }
-  
-  export interface AppConfig {
-    env?: Environment;
-    appVersion: string;
-    appBranch: string;
-    cookieKey: string;
-    apiBaseUrl: string;
-  }
-  
-  export const AppConfig: AppConfig = {
-    env: (process.env._ENV as Environment) || Environment.DEV,
-    appVersion: (process.env._VERSION as string) || "local",
-    appBranch: (process.env.BRANCH as string) || "local",
-    cookieKey: "fest_nat_data",
-    apiBaseUrl:
-      (process.env.API_BASE_URL as string) ,
-  
-  };
-  
+  DEV_SIMULATOR = "dev-simulator",
+  DEV = "dev",
+  PROD = "prod",
+  STAGING = "staging",
+}
+
+export interface AppConfig {
+  env?: Environment;
+  appVersion: string;
+  appBranch: string;
+  cookieKey: string;
+  apiBaseUrl: string;
+}
+
+export const AppConfig: AppConfig = {
+  env: (process.env.NEXT_PUBLIC_ENV as Environment) || Environment.DEV,
+  appVersion: (process.env.NEXT_PUBLIC_VERSION as string) || "local",
+  appBranch: (process.env.NEXT_PUBLIC_BRANCH as string) || "local",
+  cookieKey: "fest_nat_data",
+  apiBaseUrl: (process.env.NEXT_PUBLIC_API_BASE_URL as string),
+};
